4cc25b656d7ab157b76d0a26b5e2039856e21e81
Liam Huang <liamhuang0205+git@gmail.com>
2014-05-27 22:38:12 +0800

    paths

--- make.bat
+++ make.bat
@@ -1,5 +1,39 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lot /s
+del *.lof /s
+del *.ind /s
+del *.ilg /s
+del *.idx /s
+del *.xdy /s
+del *.ist /s
+
 xelatex --shell-escape GB-template
 bibtex GB-template
 xelatex --shell-escape GB-template
 xelatex --shell-escape GB-template
-start GB-template
+
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lot /s
+del *.lof /s
+del *.ind /s
+del *.ilg /s
+del *.idx /s
+del *.xdy /s
+del *.ist /s
+
+
+start GB-template.pdf
